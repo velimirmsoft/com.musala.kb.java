1f9f13fedad821e03098ce7981f106e0adef5a9a
Velimir Avramovski <vmsoft@gmail.com>
2018-05-11 14:55:46 +0200

    Generic things.

--- GenericThings/src/com/welle/generic/one/MainClass.java
+++ GenericThings/src/com/welle/generic/one/MainClass.java
@@ -8,11 +8,13 @@ import java.util.Collection;
 public class MainClass {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Generic Type exercise 1 ...");
 
 		Collection<Integer> cI = Arrays.asList(1, 2, 3, 4);
 		int count = ClassOne.countOddElementsInCollection(cI, new checkOdd());
 		System.out.println("Number of odd integers = " + count);
+		
 	}
 
 }
